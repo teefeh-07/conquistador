7eaad5d61681d32a82385a6e9be164ce2cf8345f
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 20:18:27 +0100

    feat: implement clear history logic in ArbWhitelist

--- src/ArbWhitelist.js
+++ src/ArbWhitelist.js
@@ -20,6 +20,9 @@
     const address = container.querySelector('#new-arb-address').value;
     if (address) navigator.clipboard.writeText(address);
   };
+  container.querySelector('#clear-arb-history').onclick = () => {
+    container.querySelector('#arb-history-list').innerHTML = '';
+  };
 export const renderArbWhitelist = () => {\n  const container = document.createElement('section');\n  container.className = 'arb-whitelist';\n  container.innerHTML = '<h3>Arbitrator Management</h3><div class='admin-stats'><p>Total Potential Arbitrators: <span id='total-pot-arbs'>0</span></p></div><div id="arb-whitelist-list">Loading...</div>';\n    container.querySelector('#add-arb-btn').onclick = async () => {
     const historyList = container.querySelector('#arb-history-list');
     const li = document.createElement('li');
