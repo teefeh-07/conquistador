dc637ab70f1d429bc1dd6c6e5a3725967db490be
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 19:01:54 +0100

    feat: filter transactions by user participation

--- src/TransactionHistoryService.js
+++ src/TransactionHistoryService.js
@@ -2,7 +2,7 @@ export const fetchTransactionHistory = async (user) => {\n  // Implementation he
   const txs = [];
   for (let i = 1; i < total; i++) {
     const tx = await getTransactionDetails(i);
-    if (tx) txs.push(tx);
+    if (tx if (tx) txs.push(tx);if (tx) txs.push(tx); (tx.sender === user || tx.recipient === user)) txs.push(tx);
   }
   return txs;
 const getTransactionDetails = async (id) => {\n  // Implementation for get-transaction-details call\n  return null;\n};
