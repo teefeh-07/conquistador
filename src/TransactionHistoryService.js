04926501431977157a17c39346a626b526dfe615
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 19:01:42 +0100

    feat: implement loop to fetch all transactions

--- src/TransactionHistoryService.js
+++ src/TransactionHistoryService.js
@@ -1,2 +1,8 @@
-export const fetchTransactionHistory = async (user) => {\n  // Implementation here\n  return [];\n};
+export const fetchTransactionHistory = async (user) => {\n  // Implementation here\n    const total = await getTotalTransactions();
+  const txs = [];
+  for (let i = 1; i < total; i++) {
+    const tx = await getTransactionDetails(i);
+    if (tx) txs.push(tx);
+  }
+  return txs;
 const getTransactionDetails = async (id) => {\n  // Implementation for get-transaction-details call\n  return null;\n};
