1ca876c29d484778e20b322de875453acc83c576
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 20:17:02 +0100

    feat: add admin stats placeholder to ArbWhitelist

--- src/ArbWhitelist.js
+++ src/ArbWhitelist.js
@@ -12,7 +12,7 @@
     const { deactivateArbitratorOnChain } = await import('./ContractInteractions.js');
     await deactivateArbitratorOnChain(address);
   };
-export const renderArbWhitelist = () => {\n  const container = document.createElement('section');\n  container.className = 'arb-whitelist';\n  container.innerHTML = '<h3>Arbitrator Management</h3><div id="arb-whitelist-list">Loading...</div>';\n    container.querySelector('#add-arb-btn').onclick = async () => {
+export const renderArbWhitelist = () => {\n  const container = document.createElement('section');\n  container.className = 'arb-whitelist';\n  container.innerHTML = '<h3>Arbitrator Management</h3><div class='admin-stats'><p>Total Potential Arbitrators: <span id='total-pot-arbs'>0</span></p></div><div id="arb-whitelist-list">Loading...</div>';\n    container.querySelector('#add-arb-btn').onclick = async () => {
     const address = container.querySelector('#new-arb-address').value;
     if (!address) return;
     const { addArbitratorOnChain } = await import('./ContractInteractions.js');
