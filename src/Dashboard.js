e800ba97f022e4afb2575d47aa3acc87885d823f
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 19:49:17 +0100

    feat: update dashboard greeting

---
+++ src/Dashboard.js
@@ -0,0 +1,1 @@
+export const renderDashboard = () => {\n  const dashboard = document.createElement('main');\n  dashboard.className = 'dashboard';\n  dashboard.innerHTML = '<h2>Welcome Back, Conquistador</h2>';\n  return dashboard;\n};
