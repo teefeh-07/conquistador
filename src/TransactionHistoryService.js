6b459c35095965293313239496b43f143e7c4187
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 19:01:28 +0100

    feat: add getTransactionDetails helper skeleton

--- src/TransactionHistoryService.js
+++ src/TransactionHistoryService.js
@@ -1,3 +1,2 @@
 export const fetchTransactionHistory = async (user) => {\n  // Implementation here\n  return [];\n};
-import { callReadOnlyFunction } from '@stacks/transactions';
-const getTotalTransactions = async () => {\n  // Implementation for get-total-transactions call\n  return 0;\n};
+const getTransactionDetails = async (id) => {\n  // Implementation for get-transaction-details call\n  return null;\n};
