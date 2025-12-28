79b2e89515d69d93349d0104152553d76b48e5ba
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 20:01:07 +0100

    feat: integrate notification logging into showNotification

--- src/Notifications.js
+++ src/Notifications.js
@@ -1,1 +1,3 @@
+import { addNotification } from './NotificationHistory.js';
 export const showNotification = (msg, type = 'info') => {\n  const notif = document.createElement('div');\n  notif.className = `notification ${type}`;\n  notif.innerText = msg;\n  document.body.appendChild(notif);\n  setTimeout(() => notif.remove(), 3000);\n};
+  addNotification(msg, type);
