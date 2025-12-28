eda1423f14e2dc17495cdbc16bf3ac67998918cd
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 19:36:01 +0100

    feat: init Footer component

---
+++ src/Footer.js
@@ -0,0 +1,1 @@
+export const renderFooter = () => {\n  const footer = document.createElement('footer');\n  footer.innerHTML = '<p>&copy; 2025 Conquistador. All rights reserved.</p>';\n  return footer;\n};
