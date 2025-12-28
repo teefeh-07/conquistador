9191cac7e8d618ef5fe09bd7cccf68d780f3444c
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 19:37:43 +0100

    test: init milestone management test file

---
+++ tests/milestone.test.ts
@@ -0,0 +1,1 @@
+import { describe, it, expect } from 'vitest';\n\ndescribe('Milestone Management Tests', () => {\n  it('should allow sender to add a milestone', () => {\n    expect(true).toBe(true);\n  });\n});
