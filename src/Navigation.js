0673cf7699f34d6aa181cb7c46dbfd6201cae571
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 20:14:56 +0100

    feat: add Admin link to navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,1 +1,1 @@
-export const renderNav = () => {\n  const nav = document.createElement('nav');\n  nav.innerHTML = '<ul><li>Dashboard</li><li>Transactions</li><li>Profile</li><li id='nav-notifs'>Log</li><li id='nav-help'>Help</li></ul>';\n  return nav;\n};
+export const renderNav = () => {\n  const nav = document.createElement('nav');\n  nav.innerHTML = '<ul><li>Dashboard</li><li>Transactions</li><li>Profile</li><li id='nav-notifs'>Log</li><li id='nav-help'>Help</li><li id='nav-admin'>Admin</li></ul>';\n  return nav;\n};
