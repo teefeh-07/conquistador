85c3184764dc0cc26ea1c74c5c1657064613ab7c
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 19:33:59 +0100

    feat: init StatsService for global metrics

---
+++ src/StatsService.js
@@ -0,0 +1,1 @@
+import { callReadOnlyFunction } from '@stacks/transactions';\n\nexport const fetchGlobalStats = async () => {\n  // Aggregated stats logic here\n  return { totalTx: 0, totalDisputes: 0 };\n};
