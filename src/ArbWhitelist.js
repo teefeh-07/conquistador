f4ecac750a7bcea863176ec9d7cacc9ae14fb732
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-28 20:14:46 +0100

    feat: implement add-arbitrator logic in component

--- src/ArbWhitelist.js
+++ src/ArbWhitelist.js
@@ -1,1 +1,7 @@
-export const renderArbWhitelist = () => {\n  const container = document.createElement('section');\n  container.className = 'arb-whitelist';\n  container.innerHTML = '<h3>Arbitrator Management</h3><div id="arb-whitelist-list">Loading...</div>';\n  return container;\n};
+export const renderArbWhitelist = () => {\n  const container = document.createElement('section');\n  container.className = 'arb-whitelist';\n  container.innerHTML = '<h3>Arbitrator Management</h3><div id="arb-whitelist-list">Loading...</div>';\n    container.querySelector('#add-arb-btn').onclick = async () => {
+    const address = container.querySelector('#new-arb-address').value;
+    if (!address) return;
+    const { addArbitratorOnChain } = await import('./ContractInteractions.js');
+    await addArbitratorOnChain(address);
+  };
+  return container;\n};
